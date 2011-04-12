c425e527fce84071eb078a8fa463e4c517110433
Julie Hansbrough <jhansbrough@college.harvard.edu>
2011-04-12 02:15:45 -0400

    first commit

---
+++ itunesparser.rb
@@ -0,0 +1,60 @@
+# usage: ruby itunesparser.rb infile outfile
+
+def readFile infile
+	# will contain all data we're interested in
+	bigArray = []
+	
+	# for each line, if there's a match to song name, put
+	# song in bigArray
+	# if we reach playlists, then stop
+	File.readlines(infile).each do |line|
+		if /<key>Name<\/key><[a-z]*>(.*)<\/[a-z]*>/.match(line) != nil
+			bigArray << /<key>Name<\/key><[a-z]*>(.*)<\/[a-z]*>/.match(line)[1]
+		elsif /<key>Playlists<\/key>/.match(line) != nil
+			return bigArray
+		end
+	end
+	
+	return bigArray
+end
+
+def exportFile(results,outfile)
+	File.open(outfile,'w') do |line|
+		results.each do |item|
+			line.puts(item)
+		end
+	end
+end
+
+def main
+	if ARGV[0] == nil || ARGV[1] == nil
+		puts "Usage: ruby itunesparser.rb infile outfile"
+		return
+	else
+		infile = ARGV[0]
+		outfile = ARGV[1]
+	end
+	
+	if File.exists?(infile) == false
+		puts "Invalid file name!"
+		return
+	end
+	
+	# Currently only outputs list of song titles — will include
+	# this menu later
+	# puts "Enter the appropriate number based on what you would like listed."
+	# puts "(1) Song names"
+	# puts "(2) Artist names"
+	# puts "(3) Composer names"
+	# puts "(4) Album names"
+	# puts "(5) Genre names"
+	# puts "(6) Total listening time"
+	
+	results = readFile infile
+	
+	exportFile(results,outfile)
+
+	return
+end
+
+main
